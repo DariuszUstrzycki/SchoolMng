d66a301c2dc5781fc79f523c6390b1ebf0e32041
darek <darek@darek-stacjonar>
2016-11-20 11:25:51 +0100

    small tested change

--- SchoolMng/src/com/ustrzycki/domain/Form.java
+++ SchoolMng/src/com/ustrzycki/domain/Form.java
@@ -6,8 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.security.auth.Subject;
-
 public class Form extends AddableToDb{
 
 	private String name = null;
